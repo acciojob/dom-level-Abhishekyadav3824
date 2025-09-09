a387b38d90984b6b0d8a42cfc3e9d55077317a59
Abhishekyadav3824 <aradaabhishek@gmail.com>
2025-09-09 10:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+
+// Start counting from 1 (the element itself)
+let level = 1;
+
+// Traverse up until <html> (which has no parentElement)
+while (element.parentElement) {
+  element = element.parentElement;
+  level++;
+}
+
+// Show result
+alert("The level of the element is: " + level);
